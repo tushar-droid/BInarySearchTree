4c5ff08621a893e24e3a7842ef85a303389d7c57
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-10-30 00:37:11 -0700

    added delete and find

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var tree = new Tree();
 
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
+    if (node === null ) {
       return;
     }
     if (node.right !== null) {
       prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
+    if (node.left !== null) { 
       prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
   };
@@ -24,5 +24,20 @@ var ARRAY = [1,7,4,23,8,9,4,3,5,7,9,67,6345,324];
 
 
 var root = tree.buildTree(ARRAY);
-prettyPrint(root)
+prettyPrint(root);
+tree.insert(69);
+tree.insert(32);
+tree.insert(432);
+tree.insert(653);
+tree.insert(478932);
 
+console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+prettyPrint(root);
+
+tree.delete(3);
+
+console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+prettyPrint(root);
+
+var t = tree.find(92)
+console.log('The node found was: ', t);
--- tree.js
+++ tree.js
@@ -16,6 +16,7 @@ class Tree {
     placeNodes = (arr) =>{
         var mid_ind = Math.floor(arr.length/2);
         if(mid_ind==0){
+            if(arr[mid_ind] ==undefined) return
             return new Node(arr[mid_ind])
         }
         var left = arr.slice(0,mid_ind);
@@ -23,6 +24,80 @@ class Tree {
         var nd = new Node(arr[mid_ind], this.placeNodes(left), this.placeNodes(right));
         return nd;
     }
+
+    insert = (value) =>{
+        this.#insertRec(this.root,value);
+    }
+
+    #insertRec = (node_val, value) =>{
+
+        if(value<node_val.data) {
+            if(node_val.left == null){
+                var nd = new Node(value)
+                node_val.left = nd;
+                return
+            }
+            else{
+               this.#insertRec(node_val.left, value)
+            }
+        }
+        else if(value>node_val.data){
+            if(node_val.right ==null){
+                var nd = new Node(value)
+                node_val.right = nd;
+                return
+            }
+            else{
+                this.#insertRec(node_val.right, value)
+            }
+        }
+
+    }
+
+    delete = (value) =>{
+        this.#deleteRec(this.root, value)
+        
+    }
+
+    #deleteRec = (node, value) =>{
+        if(node.data ===null) return null;
+        if(node.data < value) node.right = this.#deleteRec(node.right, value);
+        else if( node.data > value) node.left = this.#deleteRec(node.left, value)  
+        else if(node.data ===value){
+            if(node.left ===null) return node.right;
+            else if(node.right === null) return node.left;    
+            else{
+                let curr_node = node;           //make a copy of current node
+                node = node.right;              //go to the right node
+                while(node.left != null){       //find the lowest value in the right subtree
+                    node = node.left;           //keep on going left until you are on the last node (which will be the min.)
+                }
+                curr_node.data = node.data;     //paste the min value in the current node data 
+                this.#deleteRec(curr_node.right, curr_node.data)    //delete the min node.
+                return curr_node                //return the current node to be put as the appropriate child 
+            }        
+        }
+        return node;
+    }
+
+    find = (value) =>{
+        const val = this.#findRec(this.root, value);
+        return val? val: 'NO SUCH VALUE FOUND';
+    }
+
+
+    #findRec = (node, value) =>{
+        if(node === null) return null;
+        if(node.data < value) var val = this.#findRec(node.right, value);
+        else if (node.data > value) var val = this.#findRec(node.left, value);
+        else if( node.data === value) {
+            return node;
+        }
+        return val;
+    }
+
+
+
 
 
 
